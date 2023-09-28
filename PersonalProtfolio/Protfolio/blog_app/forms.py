3352e3608d1a8613771e69f4852bdb5755477162
sagorluc <mdsagorluc@gmail.com>
2023-09-28 15:59:04 +0600

    Done

--- PersonalProtfolio/Protfolio/blog_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from blog_app.models import Blog
-
-class BlogForm(forms.ModelForm):
-    class Meta:
-        model = Blog
-        fields = ['user', 'blog_type', 'image', 
-                  'title', 'short_text', 'description']
--- PersonalProtfolio/Protfolio/profile_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from profile_app.views import about_me, my_protfolio
-
-urlpatterns = [
-    path('', about_me, name="about"),
-    path('portfolio/', my_protfolio, name="portfolio"),
-]
